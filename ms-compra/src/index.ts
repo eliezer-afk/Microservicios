6d67f69dfb31479e97084c20aee8fc3bc7c0192a
Eliezer <eliriper123@gmail.com>
2024-10-16 14:21:45 -0300

    first commit

---
+++ ms-compra/src/index.ts
@@ -0,0 +1,8 @@
+import server from "./server";
+import colors from 'colors'
+
+const port = process.env.PORT || 4000
+
+server.listen(port, () => {
+    console.log(colors.bgCyan(`REST API en el puesto ${port}`))
+})
